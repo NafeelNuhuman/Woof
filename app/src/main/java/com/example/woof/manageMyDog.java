247c21e574bf379e4eba5a691fe2d2d0720ecbbc
Nimran <65820501+NimranH@users.noreply.github.com>
2020-09-17 18:22:57 +0530

    redid ui colors and added login page and manage my dog page

---
+++ app/src/main/java/com/example/woof/manageMyDog.java
@@ -0,0 +1,14 @@
+package com.example.woof;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class manageMyDog extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_manage_my_dog);
+    }
+}
