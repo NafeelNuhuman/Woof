dfe78c59acda404e7db5579764051815a7c71151
Nafeel <65773614+NafeelNuhuman@users.noreply.github.com>
2020-09-22 21:23:14 +0530

    added DBHelper.java

---
+++ app/src/main/java/com/example/woof/database/DBHelper.java
@@ -0,0 +1,25 @@
+package com.example.woof.database;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+import androidx.annotation.Nullable;
+
+public class DBHelper extends SQLiteOpenHelper {
+    public static final String DATABASE_NAME = "Woof.db";
+
+    public DBHelper(@Nullable Context context) {
+        super(context, DATABASE_NAME, null, 1);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase sqLiteDatabase) {
+
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase sqLiteDatabase, int i, int i1) {
+
+    }
+}
