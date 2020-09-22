22115567ee4e72d16647865ecdc10c906a111c12
Nimran <65820501+NimranH@users.noreply.github.com>
2020-09-22 12:08:30 +0530

    connected all

--- app/src/main/java/com/example/woof/SignUp.java
+++ app/src/main/java/com/example/woof/SignUp.java
@@ -2,7 +2,9 @@ package com.example.woof;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class SignUp extends AppCompatActivity {
 
@@ -10,5 +12,10 @@ public class SignUp extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
+    }
+
+    public void moveToLogin(View view){
+        Intent intent = new Intent (this,Login.class);
+        startActivity(intent);
     }
 }
