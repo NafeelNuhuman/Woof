d31f70762ead2c67ec9301002eae32c9675774d0
Nafeel <65773614+NafeelNuhuman@users.noreply.github.com>
2020-09-22 11:46:34 +0530

    added cart

---
+++ app/src/main/java/com/example/woof/other/cart.java
@@ -0,0 +1,16 @@
+package com.example.woof.other;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.woof.R;
+
+public class cart extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_cart);
+    }
+}
