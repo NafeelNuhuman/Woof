00ae9fc9953361b995b4324f1f408cd97638a00c
Nafeel <65773614+NafeelNuhuman@users.noreply.github.com>
2020-09-24 20:18:42 +0530

    completed register options page

--- app/src/main/java/com/example/woof/database/DBHelper.java
+++ app/src/main/java/com/example/woof/database/DBHelper.java
@@ -23,13 +23,13 @@ public class DBHelper extends SQLiteOpenHelper {
         //Pet Owner table
         String CREATE_TABLE_PET_OWNER =
                 "CREATE TABLE " + petOwnerMaster.petOwner.TABLE_NAME + "(" +
-                petOwnerMaster.petOwner.COLUMN_ID + " INTEGER PRIMARY KEY AUTOINCREMENT, " +
-                petOwnerMaster.petOwner.COLUMN_FNAME + " TEXT, " +
-                petOwnerMaster.petOwner.COLUMN_LNAME + " TEXT, " +
-                petOwnerMaster.petOwner.COLUMN_EMAIL + " TEXT, " +
-                petOwnerMaster.petOwner.COLUMN_CONTACT + " INTEGER, " +
-                petOwnerMaster.petOwner.COLUMN_DOB + " TEXT, " +
-                petOwnerMaster.petOwner.COLUMN_PWD + " TEXT)";
+                        petOwnerMaster.petOwner.COLUMN_ID + " INTEGER PRIMARY KEY AUTOINCREMENT, " +
+                        petOwnerMaster.petOwner.COLUMN_FNAME + " TEXT, " +
+                        petOwnerMaster.petOwner.COLUMN_LNAME + " TEXT, " +
+                        petOwnerMaster.petOwner.COLUMN_EMAIL + " TEXT, " +
+                        petOwnerMaster.petOwner.COLUMN_CONTACT + " INTEGER, " +
+                        petOwnerMaster.petOwner.COLUMN_DOB + " TEXT, " +
+                        petOwnerMaster.petOwner.COLUMN_PWD + " TEXT)";
 
         db.execSQL(CREATE_TABLE_PET_OWNER);
 
@@ -41,24 +41,24 @@ public class DBHelper extends SQLiteOpenHelper {
     }
 
     //Register pet owner
-    public boolean addPetOwner(petOwnerModel petOwnerModel){
+    public boolean addPetOwner(petOwnerModel petOwnerModel) {
         SQLiteDatabase db = this.getWritableDatabase();
         ContentValues cv = new ContentValues();
 
-        cv.put(petOwnerMaster.petOwner.COLUMN_FNAME,petOwnerModel.getfName());
-        cv.put(petOwnerMaster.petOwner.COLUMN_LNAME,petOwnerModel.getlName());
-        cv.put(petOwnerMaster.petOwner.COLUMN_EMAIL,petOwnerModel.getEmail());
-        cv.put(petOwnerMaster.petOwner.COLUMN_CONTACT,petOwnerModel.getContact());
-        cv.put(petOwnerMaster.petOwner.COLUMN_DOB,petOwnerModel.getDateOfBirth());
-        cv.put(petOwnerMaster.petOwner.COLUMN_PWD,petOwnerModel.getPwd());
+        cv.put(petOwnerMaster.petOwner.COLUMN_FNAME, petOwnerModel.getfName());
+        cv.put(petOwnerMaster.petOwner.COLUMN_LNAME, petOwnerModel.getlName());
+        cv.put(petOwnerMaster.petOwner.COLUMN_EMAIL, petOwnerModel.getEmail());
+        cv.put(petOwnerMaster.petOwner.COLUMN_CONTACT, petOwnerModel.getContact());
+        cv.put(petOwnerMaster.petOwner.COLUMN_DOB, petOwnerModel.getDateOfBirth());
+        cv.put(petOwnerMaster.petOwner.COLUMN_PWD, petOwnerModel.getPwd());
 
         long insert = db.insert(petOwnerMaster.petOwner.TABLE_NAME, null, cv);
 
         return insert != -1;
     }
 
-    //getting emails or passwords for sign in
-    public List readEmailorPwd(String req){
+    //getting emails and passwords for sign in
+    public List readEmailorPwd(String req) {
         SQLiteDatabase db = getReadableDatabase();
 
         String[] projection = {
@@ -79,7 +79,7 @@ public class DBHelper extends SQLiteOpenHelper {
         List emails = new ArrayList();
         List passwords = new ArrayList();
 
-        while (cursor.moveToNext()){
+        while (cursor.moveToNext()) {
             String email = cursor.getString(cursor.getColumnIndexOrThrow(petOwnerMaster.petOwner.COLUMN_EMAIL));
             String pwd = cursor.getString(cursor.getColumnIndexOrThrow(petOwnerMaster.petOwner.COLUMN_PWD));
             emails.add(email);
--- app/src/main/java/com/example/woof/other/registerOptions.java
+++ app/src/main/java/com/example/woof/other/registerOptions.java
@@ -5,18 +5,38 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 
 import com.example.woof.R;
 
 public class registerOptions extends AppCompatActivity {
 
+    Button btnPetOwner, btnSeller;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register_options);
+
+        btnPetOwner = findViewById(R.id.btnPetOwner);
+        btnSeller = findViewById(R.id.btnSeller);
+
+        btnPetOwner.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(registerOptions.this, register.class);
+                startActivity(intent);
+            }
+        });
+
+        btnSeller.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(registerOptions.this, registerSeller.class);
+                startActivity(intent);
+            }
+        });
+
     }
-    public void movePage(View view){
-        Intent intent = new Intent(  this,register.class);
-        startActivity(intent);
-    }
+
 }
