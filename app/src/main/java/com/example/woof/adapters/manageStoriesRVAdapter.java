6f424a688615ce1e28ad3962ee693b51db1955dd
Nethmini801 <nisansalanethmini123@gmail.com>
2020-10-04 19:32:56 +0530

    Stories fully done

--- app/src/main/java/com/example/woof/adapters/manageStoriesRVAdapter.java
+++ app/src/main/java/com/example/woof/adapters/manageStoriesRVAdapter.java
@@ -1,10 +1,13 @@
 package com.example.woof.adapters;
 
 import android.app.Activity;
+import android.app.AlertDialog;
 import android.content.Context;
+import android.content.DialogInterface;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
@@ -13,9 +16,11 @@ import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.woof.R;
 import com.example.woof.database.DBHelper;
+import com.example.woof.stories.mystories;
 
 import java.util.ArrayList;
 
+import static android.os.Build.ID;
 import static java.lang.String.valueOf;
 
 public class manageStoriesRVAdapter extends RecyclerView.Adapter<manageStoriesRVAdapter.MyViewHolder> {
@@ -23,15 +28,16 @@ public class manageStoriesRVAdapter extends RecyclerView.Adapter<manageStoriesRV
     private Context context;
     private ArrayList StoryTitle,StoryDesc;
     private  ArrayList StoryID;
+    private int storyID;
     private  String userName;
     private Activity activity;
     DBHelper dbHelper;
 
     public manageStoriesRVAdapter(Context context, ArrayList storyTitle, ArrayList storyDesc, ArrayList storyID) {
         this.context = context;
-        StoryTitle = storyTitle;
-        StoryDesc = storyDesc;
-        StoryID = storyID;
+        this.StoryTitle = storyTitle;
+        this.StoryDesc = storyDesc;
+        this.StoryID = storyID;
     }
 
 
@@ -47,9 +53,16 @@ public class manageStoriesRVAdapter extends RecyclerView.Adapter<manageStoriesRV
     public void onBindViewHolder(@NonNull MyViewHolder holder, final int position) {
         dbHelper = new DBHelper(context);
         String userID  = dbHelper.getUserID((Integer) StoryID.get(position));
-
+        storyID = (Integer)StoryID.get(position);
         holder.StoryTitle_txt.setText(valueOf(StoryTitle.get(position)));
         holder.StoryDesc_txt.setText(valueOf(StoryDesc.get(position)));
+
+        holder.deleteBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                confirmDelete();
+            }
+        });
     }
 
     @Override
@@ -60,6 +73,7 @@ public class manageStoriesRVAdapter extends RecyclerView.Adapter<manageStoriesRV
     public class MyViewHolder extends RecyclerView.ViewHolder {
         TextView StoryTitle_txt,StoryDesc_txt;
         CardView card;
+        Button deleteBtn;
 
 
         public MyViewHolder(@NonNull View itemView) {
@@ -67,8 +81,30 @@ public class manageStoriesRVAdapter extends RecyclerView.Adapter<manageStoriesRV
             StoryTitle_txt=itemView.findViewById(R.id.MyStoryTiTLE);
             StoryDesc_txt=itemView.findViewById(R.id.MyStoryDesc);
             card = itemView.findViewById(R.id.MystorySingleCard);
+            deleteBtn = itemView.findViewById(R.id.StoryDeleteBtn);
 
 
         }
+    }
+    void confirmDelete(){
+        AlertDialog.Builder builder=new AlertDialog.Builder(context);
+        builder.setTitle("Delete story?");
+        builder.setMessage("Are yo sure you want to delete this story?" );
+        builder.setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                DBHelper dbHelper =new DBHelper(context);
+                dbHelper.DeleteOneRowOfStories(String.valueOf(storyID));
+                activity.finish();
+
+            }
+        });
+        builder.setNegativeButton("No", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+
+            }
+        });
+        builder.create().show();
     }
 }
